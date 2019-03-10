f612e2476054269b5a68e17c40a04e7f289828dd
pawel <mark.dennis@tsgforce.com>
2019-03-10 19:06:41 -0400

    Added a commentBox test with a fullDOM render from enzyme for testing purposes and to get experience with fullDOM render method | Need to unmount component after each test runs in the fullDOM render test suite in order to not conflict with the other running tests in the suite

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-    return <div>Comment Box</div>
+class CommentBox extends Component {
+  
+    state = {
+        comment: ''
+    }
+  
+    handleChange = (event) => {
+        console.log("On change fired with: " + event.target.value);
+        this.setState( { comment: event.target.value })
+    };
+
+    handleSubmit = event => {
+        // Prevent the default behavior of a form element, which is to reload the page on submission
+        event.preventDefault();
+
+        // TODO - will eventually call an action creator with React Redux.
+        // Currently the application is not wired in with Redux, therefore the Component's state will be updated.
+        this.setState( { comment: '' });
+    };
+
+    render() {
+       return (
+            <form onSubmit={this.handleSubmit}>
+                <h4>Add a Comment</h4>
+                <textarea onChange={this.handleChange} value={this.state.comment}/>
+                <div>
+                    <button>Submit Comment</button>
+                </div>
+            </form>
+       );
+   } 
 }
+
+export default CommentBox;
