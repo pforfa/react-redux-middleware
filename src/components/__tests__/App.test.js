de97187d34d4b36fbca678dc5f72af01e28d15b4
pawel <mark.dennis@tsgforce.com>
2019-03-10 18:41:26 -0400

    Added a .env variable to enable easier import statements refactoring | Refactored App.test.js to include a beforeEach() method for instantiating an instance of the <App/> component.

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+/* 
+App component will get initialized in beforeEach() function
+*/
+let wrappedComponent;
+
+beforeEach(() => {
+    wrappedComponent = shallow(<App />);
+});
 
 it ('shows a comment box', () => {
-
-    const wrappedComponent = shallow(<App />);
-
     expect(wrappedComponent.find(CommentBox).length).toEqual(1);
 
 
 });
+
+it ('shows one instance of comment list', () => {
+    expect(wrappedComponent.find(CommentList).length).toEqual(1);
+})
