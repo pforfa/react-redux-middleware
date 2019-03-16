012eece19ffe0b5136400bc16c527f13a8cd23ec
Pawel Forfa <siwy19921992@gmail.com>
2019-03-16 18:14:47 -0400

    Refactored the comment list component to become a functional component, and wired up redux into it to utilize actions/reducers .

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React, { Component }from 'react';
+import { connect } from 'react-redux';
 
-export default () => {
-    return <div>Comment List</div>;
+class CommentList extends Component {
+
+    renderComments() {
+        return this.props.comments.map(comment => {
+            return (
+                <li>
+                    {comment}
+                </li>
+            )
+        });
+    }
+
+    render () {
+        return (
+        <div>
+            <ul>
+                {this.renderComments()}
+            </ul>
+        </div>
+        );
+    }
 }
+
+function mapStateToProps(state) {
+    // Return state.comments, since the reducers/index.js
+    // exports a reducers called 'comments' in the combinedReducers() function
+    return { comments: state.comments }
+}
+
+export default connect(mapStateToProps)(CommentList);
