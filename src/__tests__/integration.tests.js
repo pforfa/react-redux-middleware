10f3d02f029911016538950d4431ded06aede6bc
Pawel Forfa <siwy19921992@gmail.com>
2019-03-17 11:01:29 -0400

    Added logic for integration tests to use moxios | created a setTimeout function to allow time for moxios to intercept requests from the fetchComments() function in actionCreators

--- src/__tests__/integration.tests.js
+++ src/__tests__/integration.tests.js
@@ -1,9 +1,30 @@
 import  React from 'react';
 import { mount } from 'enzyme';
+import moxios from 'moxios';
 import Root from 'Root';
 import App from 'components/App';
 
-it('can fetch a list of comments and display them', () => {
+beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [
+            {
+                name: 'Fetched comment #1'
+            },
+            {
+                name: 'Fetched comment #2'
+            }
+        ]
+    });
+});
+
+afterEach(() => {
+    moxios.uninstall();
+});
+
+// Add a callback function which jest expects to be called if there is a delay inside the test, such as a setTimeout function
+it('can fetch a list of comments and display them', (done) => {
     // Render the whole application
     const wrappedComponent = mount(
         <Root>
@@ -14,6 +35,20 @@ it('can fetch a list of comments and display them', () => {
     // Find the button for fetching comments from the place holder API
     wrappedComponent.find('.fetch-comments').simulate('click');
 
-    expect(wrappedComponent.find('li').length).toEqual(500);
+    // Introducer a small delay to allow moxios (axios request mocking library) to intercept the
+    // request fired from the fetchComments action creator
+    setTimeout(() => {
+        wrappedComponent.update();
+
+        expect(wrappedComponent.find('li').length).toEqual(2);
+
+        // Call the done() callback function in order to signal Jest that the test finishes here.
+        // Note that this is a setTimeout function, which causes a delay in testing.
+        // Jest tries to finish the test as quickly as possible, and if this done() function isn't present,
+        // the test will most likely fail because it won't take into consideration the result that the callback function inside of
+        // setTimeout may produce.
+        done();
+        wrappedComponent.unmount();
+    }, 500);
 
 });
