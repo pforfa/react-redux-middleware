a06d18a1347ea600ed2a263065bd2f023ecbce24
Pawel Forfa <siwy19921992@gmail.com>
2019-03-17 08:52:55 -0400

    Updated CommentBox test to accommodate the additional 'Fetch Comments' button that was added

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -17,9 +17,9 @@ afterEach(() => {
    wrappedComponent.unmount(); 
 });
 
-it('has a text area and a button', () => {
+it('has a text area and two buttons', () => {
     expect(wrappedComponent.find('textarea').length).toEqual(1);
-    expect(wrappedComponent.find('button').length).toEqual(1);
+    expect(wrappedComponent.find('button').length).toEqual(2);
 });
 
 describe('the text area', () => {
