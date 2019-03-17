9c327090b003b39cb3f31face56850d537560435
Pawel Forfa <siwy19921992@gmail.com>
2019-03-17 13:36:18 -0400

    Created action creator and reducer for authentication | Added react-router-dom and wrapped the main app component with <BrowserRouter> | Created Routes in the <App> component to separate posting and retrieving comments

--- src/setupTests.js
+++
@@ -1,5 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-// Basic setup configuration to allow the use of enzyme in Unit/Regression Testing.
-Enzyme.configure({adapter: new Adapter() });
