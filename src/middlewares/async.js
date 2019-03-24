bdf4aaa9d09d53b643ea1977f270fdf5dccd23ea
Pawel Forfa <siwy19921992@gmail.com>
2019-03-24 14:22:42 -0400

    Initial middleware logic

---
+++ src/middlewares/async.js
@@ -0,0 +1,7 @@
+export default ({ dispatch }) => {
+    return function(next) { // next is a reference to the next middleware
+        return function(action) { // is a reference to the actual action from the action creator
+
+        }
+    }
+}
